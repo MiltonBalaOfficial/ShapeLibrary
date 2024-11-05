267c8d9481eb41055e2ea9af478910fd22b8d43b
MiltonBalaOfficial <118467836+MiltonBalaOfficial@users.noreply.github.com>
2024-11-05 18:16:59 +0530

    Add files via upload

---
+++ Update 1/New shapes/For shape dictionary (new shapes).lua
@@ -0,0 +1,12 @@
+
+                            --[5] = {
+                            --    name = "Physics-Diagrams",
+                            --    shapes = {
+                            --        [1] = { name = "Spring", shapeName = "spring" },
+                            --        [2] = { name = "Immovable Support", shapeName = "immuvableSupport" },
+                            --        [3] = { name = "Right Hand Rule", shapeName = "rightHandRule" },
+                            --        [4] = { name = "Vertical Circle", shapeName = "verticalCircle" },
+                                    [5] = { name = "Venturi Meter", shapeName = "venturimeter" },
+                                    [6] = { name = "Cyclotron", shapeName = "cyclotron" },
+                                    [7] = { name = "Car", shapeName = "car" },
+                                    [8] = { name = "Organ Pipes", shapeName = "organPipes" },
